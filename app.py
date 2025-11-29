ccc2e2973783f35b36bf8947b33713edd7c657e2
Mohith <mohithbatchala21@outlook.com>
2025-11-29 19:22:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def index():
 if __name__ == "__main__":
     # Render sets PORT env variable; default 5000 for local runs
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)
